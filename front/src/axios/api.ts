c4d309e330812e4a0a78bc9d5f6c8a2918b90156
Rafa W <54604961+rafxrad@users.noreply.github.com>
2024-04-26 14:42:13 -0300

    Update api.ts

--- front/src/axios/api.ts
+++ front/src/axios/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://ec2-18-188-176-8.us-east-2.compute.amazonaws.com/tasks",
   headers: { "Content-Type": "application/json" },
 });
 
